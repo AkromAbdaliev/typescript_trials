ab6f82ef1702d6d1f6d2f3e6abc9f455129fcfb3
Akromjon Abdaliev <akromjon996@gmail.com>
2026-01-06 06:32:10 +0000

    add parameter properties

--- src/index.ts
+++ src/index.ts
@@ -22,23 +22,80 @@
 // } = { id: 1, name: 'John Smith', born: new Date() };
 
 // OOP
-class Account {
-  id: number;
-  owner: string;
-  balance: number;
+// class Account {
+//   readonly id: number;
+//   owner: string;
+//   balance: number;
 
-  constructor(id: number, owner: string, balance: number) {
-    this.id = id;
-    this.owner = owner;
-    this.balance = balance;
+//   constructor(id: number, owner: string, balance: number) {
+//     this.id = id;
+//     this.owner = owner;
+//     this.balance = balance;
+//   }
+
+//   deposit(amount: number) {
+//     if (amount <= 0) throw new Error('Invalid amount');
+//     this.balance += amount;
+//   }
+// }
+
+// let account = new Account(12, 'Akrom', 0);
+// account.deposit(5000000);
+// console.log(`Your balance: ${account.balance}`);
+// console.log(account instanceof Account);
+
+// console.log(account.constructor.name);
+
+// let variable: string = '12345';
+// console.log((variable as string).length);
+// const num = Number(variable);
+// console.log(num);
+
+// class Person {
+//   protected name!: string;
+
+//   constructor();
+//   constructor(name: string);
+//   constructor(name?: string) {
+//     if (name) this.name = name;
+//   }
+//   public getName(): string {
+//     return this.name;
+//   }
+
+//   public setName(name: string): void {
+//     this.name = name;
+//   }
+// }
+
+// class Student extends Person {}
+
+// const student = new Student('Mike');
+// console.log(student.getName());
+
+// let person = new Person();
+// person.setName('Jonh');
+// console.log(person.getName());
+
+class Student {
+  constructor(
+    public readonly id: number,
+    public name: string,
+    private _balance: number,
+  ) {}
+
+  public deposit(amount: number) {
+    if (amount <= 0) {
+      throw new Error('Invalid amount');
+    }
+    this._balance += amount;
   }
 
-  deposit(amount: number) {
-    if (amount <= 0) throw new Error('Invalid amount');
-    this.balance += amount;
+  get balance(): number {
+    return this._balance;
   }
 }
 
-let account = new Account(12, 'Akrom', 0);
-account.deposit(5000000);
-console.log(`Your balance: ${account.balance}`);
+let student = new Student(12, 'John Doe', 0);
+student.deposit(120000);
+console.log(`John Doe's balance $${student.balance}`);
